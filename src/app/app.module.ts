9a1c3e38ffae1f83862685fdf66108e1c8095c12
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-12-03 13:29:19 +0100

    create basic structure & add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesListComponent } from './components/countries-list/countries-list.component';
+import { CountryComponent } from './components/country/country.component';
+import { CountryDetailComponent } from './components/country-detail/country-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CountriesListComponent,
+    CountryComponent,
+    CountryDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
